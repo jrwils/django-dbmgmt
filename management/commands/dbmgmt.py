928b3330b79eeb1ca037a461005c2a43ee95b901
Joe Wilson <joe.wlsn@gmail.com>
2015-02-01 12:46:06 -0800

    Adds an --all argument to dbmgmt for running all files

--- management/commands/dbmgmt.py
+++ management/commands/dbmgmt.py
@@ -2,11 +2,27 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.db import connection
 from os import listdir, path
+from optparse import make_option
 
 
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+                    make_option(
+                        '--all',
+                        action='store_true',
+                        dest='run_all',
+                        ),
+                    )
 
     def handle(self, *args, **options):
+        if not hasattr(settings, 'SQL_DIR'):
+            raise AttributeError('Please assign a SQL_DIR directory in settings.py')
+
+        run_all = options.get('run_all')
+
+        if run_all is None and len(args) == 0:
+            raise ValueError('Please pass a file argument or --all')
+
         files_rmext = [path.splitext(sqlfile)[0] for sqlfile in listdir(settings.SQL_DIR)]
         ## print a notice if any of the arguments don't exist as files in SQL_DIR
         for arg in args:
@@ -14,9 +30,10 @@ class Command(BaseCommand):
                 self.stdout.write('{} file not found in {} - this file will be skipped'.format(arg, settings.SQL_DIR))
         for file in listdir(settings.SQL_DIR):
             file_rmext = path.splitext(file)[0]
-            if not args or file_rmext in args:
+            if run_all or file_rmext in args:
                 fhandle = open(path.join(settings.SQL_DIR, file))
                 sql_txt = fhandle.read()
                 cursor = connection.cursor()
                 cursor.execute(sql_txt)
                 self.stdout.write('Updated {}'.format(file_rmext))
+                cursor.close()
